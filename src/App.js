d4da071b6567e9610a2374b4a03c6e0eabd983d9
Nurpeiil <nurpeiilk99@gmail.com>
2023-05-01 16:49:41 +0600

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [state, setSate] = useState([]);
+  const [newState, setNewState] = useState();
+
+  useEffect(()=>{
+    fetch ('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((json) => setSate(json));
+    },[]);
+    async function fetchState(id){
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${id}`
+      );
+      const responseState = await  response.json();
+      setNewState(responseState);
+      
+    }
+    useEffect(()=>{
+      fetchState()
+    },[]);
+  
+    return (
+      <Use className="App">
+        {state.map((el)=><Style onClick ={()=>fetchState(el.id)}>{el.username}</Style>)}
+        {newState&&<Color className="color">
+          <p>{newState.name}</p>
+          <p>{newState.id}</p> 
+          <p>{newState.email}</p> 
+          <p>{newState.phone}</p> 
+          </Color>}
+      </Use>
+    )
+  }
 
 export default App;
+
+const Style = styled.div`
+background-color: aqua;
+margin-top: 5px;
+border: 1px solid;
+width:200px;
+height:40px;
+text-align:center;
+`
+const Color = styled.div`
+background-color: red;
+width: 200px;
+border: 1px solid;
+`
+const Use = styled.div`
+display:flex;
+flex-direction:column;
+align-items:  center;
+`
+
+
